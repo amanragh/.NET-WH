7d9be02fad6190c58a80e02f8c56e637a46920e4
Aman <amanraghuveer99@gmail.com>
2023-03-24 15:58:15 +0530

    Add files via upload

---
+++ PrismWPFDemo/PRISMMODULES/Model/ToolbarModule.cs
@@ -0,0 +1,26 @@
+﻿using PRISMMODULES.View;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRISMMODULES.Model
+{
+    public class ToolbarModule : IModule, INotifyPropertyChanged
+    {
+        private readonly IRegionViewRegistry regionViewRegistry = null;
+        public ToolbarModule(IRegionViewRegistry regionViewRegistry)
+        {
+            this.regionViewRegistry = regionViewRegistry;
+        }
+        public void Initialize()
+        {
+            this.regionViewRegistry.RegisterViewWithRegion("HeaderRegion", typeof(ToolbarWindow));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ PrismWPFDemo/PRISMMODULES/ViewModels/ToolBarViewModel.cs
@@ -0,0 +1,32 @@
+﻿using PRISMMODULES.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRISMMODULES.ViewModels
+{
+    class ToolBarViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private ToolbarModule _TM;
+        public ToolbarModule TM
+        {
+            get
+            {
+                return _TM;
+            }
+            set
+            {
+                _TM = value;
+                RaisePropertyChanged("_TM");
+            }
+        }
+    }
+}
