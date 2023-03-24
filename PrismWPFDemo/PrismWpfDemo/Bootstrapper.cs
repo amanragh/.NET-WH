46d4ec43e34a834e892d547d18d18c2ba9802ba3
Aman <amanraghuveer99@gmail.com>
2023-03-24 15:59:40 +0530

    Add files via upload

---
+++ PrismWPFDemo/PrismWpfDemo/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismWpfDemo
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<PrismAppShell>();
+        }
+        protected override void InitializeModules()
+        {
+            base.InitializeModules();
+            App.Current.MainWindow = (PrismAppShell)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(PRISMMODULES.Model.ToolbarModule));
+        }
+    }
+}
